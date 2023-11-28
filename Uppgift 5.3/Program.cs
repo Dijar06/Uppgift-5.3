7b331e6494ecf048c3412942ee6aa72a0f7d9153
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 14:56:10 +0100

    Add project files.

---
+++ Uppgift 5.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.WriteLine("Skriv in tal nummer " + (i + 1));
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(tal[0] + tal[1] + tal[2]);
+        }
+    }
+}
